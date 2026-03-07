b9de0bcb26f7547000a7d8b5d62867f8369767b3
Will <asampongg377@gmail.com>
2026-03-07 19:30:38 +0000

    Init

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 
+dotenv.config();
+
 const app = express();
+
+// Connect to MongoDB
+if (mongoose.connection.readyState === 0) {
+  connectDB();
+}
 
 app.use(express.json());
 
